f51571cf97283fdb462a597cd5d476859b64a219
mrajkumar83 <mrajkumar83@gmail.com>
2019-12-11 01:36:10 +0100

    Menu setting
    Menu complete

---
+++ .github/workflows/contact-form-handler.php
@@ -0,0 +1,28 @@
+    
+<script language="javascript" type="text/javascript">  
+if (window.alert('Thank you for the message. We will contact you shortly.'))
+{
+    
+	
+window.history.back()
+}
+else
+{
+window.history.back()
+}
+//window.location = 'contact.html';     
+</script>    
+ 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<br />
+ Error: all fields are required
+
+</body>
+</html>
